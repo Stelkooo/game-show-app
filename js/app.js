031a5ad7c1302bc992a1ee0d3102e772f0bfc5bb
Stelko <stilyanmtoskov@gmail.com>
2021-08-28 14:32:22 +0100

    hide-start-overlay

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
 const keyboard = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
 let missed = 0;
+
+const resetButton = document.getElementsByClassName('btn__reset')[0];
+
+resetButton.addEventListener('click', (e) => {
+    const startOverlay = document.getElementById('overlay');
+    startOverlay.style.display = 'none';
+})
